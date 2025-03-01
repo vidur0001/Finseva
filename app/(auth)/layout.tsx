a98592073ddd7fc4f903becb8f16130adf5df201
Vidur Sharma <vidursharma8035@gmail.com>
2025-03-01 12:22:26 +0530

    Implement initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+export const dynamic = 'force-dynamic'
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+export const dynamic = 'force-dynamic'
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
